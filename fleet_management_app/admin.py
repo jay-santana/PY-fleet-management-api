4989a766de856995c3b3d10a472a60bf5906e591
Jay Santana <jayannyrayara@gmail.com>
2024-04-10 15:43:25 -0300

    T002: feat - Adicionar models, migrações e registros no admin

--- fleet_management_app/admin.py
+++ fleet_management_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Taxis, Trajectories
+
+admin.site.register(Taxis)
+admin.site.register(Trajectories)
--- fleet_management_app/models.py
+++ fleet_management_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Taxis(models.Model):
+  id = models.AutoField(primary_key=True)
+  plate = models.CharField(max_length=20, unique=True)
+
+  def str(self):
+    return f"ID: {self.id}, Plate: {self.plate}"
+    
+class Trajectories(models.Model):
+  id = models.AutoField(primary_key=True)
+  date = models.DateTimeField()
+  latitude = models.FloatField()
+  longitude = models.FloatField()
+  taxi = models.ForeignKey(Taxis, on_delete=models.CASCADE, related_name='trajectories')
+
+  def str(self):
+    return f"Trajectory ID: {self.id}, Date: {self.date}, Latitude: {self.latitude}, Longitude: {self.longitude}, Taxi: {self.taxi}"
