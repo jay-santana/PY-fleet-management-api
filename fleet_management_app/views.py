aae06693811b8c9560c9311f8476516dd4346762
Jay Santana <jayannyrayara@gmail.com>
2024-04-12 12:42:51 -0300

    T003: feat - Configurar paginação para lista de taxis e trajectories

--- fleet_management_app/views.py
+++ fleet_management_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination 
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -14,17 +15,23 @@ import json
 @api_view(['GET'])
 def get_taxis(request):
     if request.method == 'GET':
-        taxis = Taxis.objects.all()    # Get all objects in Tax's database (It returns a queryset)
-        serializer = TaxisSerializer(taxis, many=True)     # Serialize the object data into json (Has a 'many' parameter cause it's a queryset)    
-        return Response(serializer.data)   # Return the serialized data                 
+        taxis = Taxis.objects.all()    # Get all objects in Taxis database (It returns a queryset)
+        paginator = PageNumberPagination()
+        paginator.page_size = 10
+        result_page = paginator.paginate_queryset(taxis, request)
+        serializer = TaxisSerializer(result_page, many=True)     # Serialize the object data into json (Has a 'many' parameter cause it's a queryset)    
+        return paginator.get_paginated_response(serializer.data)   # Return the serialized data                 
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def get_trajectories(request):
     if request.method == 'GET':
         trajectories = Trajectories.objects.all()
-        serializer = TrajectoriesSerializer(trajectories, many=True)
-        return Response(serializer.data)
+        paginator = PageNumberPagination()
+        paginator.page_size = 10 #paginação dos dados
+        result_page = paginator.paginate_queryset(trajectories, request)
+        serializer = TrajectoriesSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 # def databaseEmDjango():
