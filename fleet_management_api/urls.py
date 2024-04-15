95040553cf46b7bd16eda92836833ea8823d178c
Jay Santana <jayannyrayara@gmail.com>
2024-04-15 12:10:22 -0300

    T004: feat - Adicionar rota para acessar o Swagger

--- fleet_management_api/urls.py
+++ fleet_management_api/urls.py
@@ -17,8 +17,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+schema_view = swagger_get_schema_view(
+  openapi.Info(
+    title="Documentação para o get de taxis",
+    default_version='1.0.0',
+    description='Api documentada',
+  ),
+  public=True,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('fleet_management_app.urls'), name='fleet_management_app.urls'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger_schema'),
 ]
 
