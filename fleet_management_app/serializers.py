eaaf03b9009bad131196f5f5b39ac236585b8a95
Jay Santana <jayannyrayara@gmail.com>
2024-04-17 12:23:22 -0300

    T005: refactor - Refatorar código do serializer

--- fleet_management_app/serializers.py
+++ fleet_management_app/serializers.py
@@ -5,9 +5,9 @@ from .models import Taxis, Trajectories
 class TaxisSerializer(serializers.ModelSerializer):
     class Meta:
         model = Taxis
-        fields = '__all__'
-
+        fields = ['id', 'plate']  # Inclua todas as informações do táxi que você deseja retornar
 class TrajectoriesSerializer(serializers.ModelSerializer):
+    taxi = TaxisSerializer()  # Use o serializer do táxi para serializar o objeto de táxi
     class Meta:
         model = Trajectories
         fields = '__all__'
