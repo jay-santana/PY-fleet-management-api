a5eed0a70e64c7c527f062eca0f5b3d7cf730975
Jay Santana <jayannyrayara@gmail.com>
2024-04-22 13:35:51 -0300

    T007: refactor - Refatorar código do endpoint listTrajectories

--- fleet_management_app/views.py
+++ fleet_management_app/views.py
@@ -9,7 +9,7 @@ from rest_framework import status
 from .models import Taxis, Trajectories
 from .serializers import TaxisSerializer, TrajectoriesSerializer
 from .schemas import taxis_list_schema, trajectories_list_schema
-from .utils import TaxiUtils
+from .utils import TaxiUtils, TrajectoriesUtils
 
 import json
 
@@ -72,19 +72,46 @@ listTaxis = taxis_list_schema(method='get')(listTaxis)
 @api_view(['GET'])
 def listTrajectories(request):
     if request.method == 'GET':
+        # Instanciar a classe TaxiUtils com o objeto de request
+        trajectories_utils = TrajectoriesUtils(request)
+        # Get request query parameters
+        filter_by = request.query_params.get('filter_by')
+        sort_by = request.query_params.get('sort_by')
+        search = request.query_params.get('search')
+
+        # Get page size and page number
+        page_size = trajectories_utils.get_page_size()
+        page_number = trajectories_utils.get_page_number()
+
+        # Filter, sort, and search taxis
         trajectories = Trajectories.objects.all()
+        trajectories = trajectories_utils.filter_trajectories(trajectories, filter_by)
+        trajectories = trajectories_utils.sort_trajectories(trajectories, sort_by)
+        trajectories = trajectories_utils.search_trajectories(trajectories, search)
+       
+        # Paginate the queryset using PageNumberPagination
         paginator = PageNumberPagination()
-        paginator.page_size = 10 #data pagination
+        paginator.page_size = page_size
         result_page = paginator.paginate_queryset(trajectories, request)
+        
+        # Verificar se a lista de táxis está vazia
+        if not result_page:
+            return Response({'detail': 'Nenhum táxi encontrado.'}, status=status.HTTP_404_NOT_FOUND)
+        
+        # Serialize the paginated queryset
         serializer = TrajectoriesSerializer(result_page, many=True)
+        
+        # Construct response data
         response = paginator.get_paginated_response(serializer.data)
+        
         # Including additional pagination information at the beginning of the response object
         pagination_info = {
             'current_page': paginator.page.number,
             'total_pages': paginator.page.paginator.num_pages
         }
-        # Adding pagination information at the beginning of the response object
+         # Add current_page and total_pages
         response.data = {**pagination_info, **response.data}
+        
         return response   # Return the serialized data 
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
