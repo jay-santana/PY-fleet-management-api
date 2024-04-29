4009b9a2433fea77b4d8e4cb99b4bbe3398de1f6
Jay Santana <jayannyrayara@gmail.com>
2024-04-29 12:53:55 -0300

    T007: feat - Adicionar rota para last_taxi_locations

--- fleet_management_app/urls.py
+++ fleet_management_app/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('taxis/', views.listTaxis, name='get_all_taxis'),
     path('trajectories/', views.listTrajectories, name='get_all_trajectories'),
+    path('last_taxi_location/', views.last_taxi_locations, name='last_taxi_location'),
 ]
