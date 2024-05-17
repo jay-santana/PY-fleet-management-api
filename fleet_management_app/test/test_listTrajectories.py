932d8c8d76d50233eef2b111b61dd1e32cec803b
Jay Santana <jayannyrayara@gmail.com>
2024-05-17 08:14:24 -0300

    T008: test - Refatorar código de testes e aumentar cobertura para listTrajectories

--- fleet_management_app/test/test_listTrajectories.py
+++ fleet_management_app/test/test_listTrajectories.py
@@ -13,8 +13,8 @@ def mock_trajectories_queryset(request):
     ]
 
     mock_trajectories = [
-        Trajectories(id=1, taxi=mock_taxis[0], date=datetime(2024, 5, 10, 5, 10, 2), latitude=10.1234, longitude=20.5678),
-        Trajectories(id=2, taxi=mock_taxis[1], date=datetime(2024, 5, 9, 9, 10, 3), latitude=11.4321, longitude=21.8765)
+        Trajectories(id=3, taxi=mock_taxis[0], date=datetime(2024, 5, 10, 5, 10, 2), latitude=10.1234, longitude=20.5678),
+        Trajectories(id=4, taxi=mock_taxis[1], date=datetime(2024, 5, 9, 9, 10, 3), latitude=11.4321, longitude=21.8765)
     ]
     with patch('fleet_management_app.views.viewsTrajectories.Trajectories.objects.all') as mock_trajectories_queryset:
         # Crie um mock do queryset de Trajectories
@@ -32,52 +32,85 @@ def test_list_trajectories_endpoint(mock_trajectories_queryset):
     assert response.status_code == 200
     assert len(response.data['results']) > 0
 
-def test_list_trajectories_filter(mock_trajectories_queryset):
+def test_list_trajectories_filter_id(mock_trajectories_queryset):
     factory = APIRequestFactory()
 
     # Making a GET request to the endpoint with filter parameter
-    request = factory.get('/api/trajectories/', {'taxi__plate': 'ABCD-1234'})
+    request = factory.get('/api/trajectories/', {'filter_by':1})
     response = listTrajectories(request)
 
     # Asserting the response data
     assert response.status_code == 200
-    assert len(response.data['results']) == 2
-    assert response.data['results'][0]['taxi']['plate'] == 'ABCD-1234'
+    assert len(response.data['results']) == 1
+    assert response.data['results'][0]['taxi']['id'] == 1
 
-def test_list_trajectories_sort(mock_trajectories_queryset):
+def test_list_trajectories_sort_desc(mock_trajectories_queryset):
     factory = APIRequestFactory()
 
     # Making a GET request to the endpoint with sort parameter
-    request = factory.get('/api/trajectories/', {'order_by': '-taxi__plate'})
+    request = factory.get('/api/trajectories/', {'sort_by': '-plate'})
     response = listTrajectories(request)
 
     # Asserting the response data
     assert response.status_code == 200
     assert len(response.data['results']) == 2
-    assert response.data['results'][1]['taxi']['plate'] == "DEFG-4567"
-    assert response.data['results'][0]['taxi']['plate'] == "ABCD-1234"
+    assert response.data['results'][0]['taxi']['plate'] == "DEFG-4567"
+    assert response.data['results'][1]['taxi']['plate'] == "ABCD-1234"
 
-def test_list_trajectires_search(mock_trajectories_queryset):
+def test_list_trajectories_sort_asc(mock_trajectories_queryset):
+    factory = APIRequestFactory()
+
+    # Making a GET request to the endpoint with sort parameter
+    request = factory.get('/api/trajectories/', {'sort_by': 'latitude'})
+    response = listTrajectories(request)
+
+    # Verificar a ordem dos resultados
+    print("Ordem dos resultados:", [item['latitude'] for item in response.data['results']])
+
+    # Asserting the response data
+    assert response.status_code == 200
+    assert len(response.data['results']) == 2
+    assert response.data['results'][0]['latitude'] == 10.1234
+    assert response.data['results'][1]['latitude'] == 11.4321
+
+def test_list_trajectires_search_plate(mock_trajectories_queryset):
     factory = APIRequestFactory()
 
     # Making a GET request to the endpoint with search parameter
-    request = factory.get('/api/trajectories/', {'taxi__plate': 'ABCD'})
+    request = factory.get('/api/trajectories/', {'search':'ABCD-1234'})
     response = listTrajectories(request)
 
     # Asserting the response status code
     assert response.status_code == 200
 
     # Asserting the response data
-    assert len(response.data['results']) == 2
+    assert len(response.data['results']) == 1
     assert response.data['results'][0]['taxi']['plate'] == 'ABCD-1234'
+
+def test_list_trajectires_search_id(mock_trajectories_queryset):
+    factory = APIRequestFactory()
+
+    # Making a GET request to the endpoint with search parameter
+    request = factory.get('/api/trajectories/', {'search':3})
+    response = listTrajectories(request)
+
+    # Printando a lista de trajetórias retornadas
+    print(response.data['results'])
+
+    # Asserting the response status code
+    assert response.status_code == 200
+
+    # Asserting the response data
+    assert len(response.data['results']) == 1
+    assert response.data['results'][0]['id'] == 3
 
 def test_list_trajectories_page_number(mock_trajectories_queryset):
     # Mocking the queryset with more than one page of data
-    mock_taxis_trajectories = [Taxis(id=i, plate=f"ABC-{i}") for i in range(1, 21)]  # Creating 20 mock taxis
+    mock_taxis = [Taxis(id=i, plate=f"ABC-{i}") for i in range(1, 21)]  # Creating 20 mock taxis
     mock_trajectories = [
         Trajectories(
             id=i, 
-            taxi=mock_taxis_trajectories[i - 1], 
+            taxi=mock_taxis[i - 1], 
             date=datetime(i,m,i,i,i,i), 
             latitude=i, 
             longitude=i
@@ -104,11 +137,11 @@ def test_list_trajectories_page_number(mock_trajectories_queryset):
 
 def test_list_trajectories_page_size(mock_trajectories_queryset):
     # Mocking the queryset with more than one page of data
-    mock_taxis_trajectories = [Taxis(id=i, plate=f"ABC-{i}") for i in range(1, 21)]  # Creating 20 mock taxis
+    mock_taxis = [Taxis(id=i, plate=f"ABC-{i}") for i in range(1, 21)]  # Creating 20 mock taxis
     mock_trajectories = [
         Trajectories(
             id=i, 
-            taxi=mock_taxis_trajectories[i - 1], 
+            taxi=mock_taxis[i - 1], 
             date=datetime(i,m,i,i,i,i), 
             latitude=i, 
             longitude=i
@@ -132,5 +165,4 @@ def test_list_trajectories_page_size(mock_trajectories_queryset):
     assert response.data['total_pages'] == 44
     assert response.data['next'] is not None
     assert response.data['previous'] is None
-
 
