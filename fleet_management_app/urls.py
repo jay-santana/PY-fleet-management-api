2a4a59a6d8ee8383334db9e72f7d9d83c893711f
Jay Santana <jayannyrayara@gmail.com>
2024-05-17 08:27:34 -0300

    T008: refactor - Atualizar nome de função na rota e views de lastTaxisLocation

--- fleet_management_app/urls.py
+++ fleet_management_app/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import viewsLastTaxisLocation, viewsTaxis, viewsTrajectories
+from .views import viewsTaxis, viewsTrajectories, viewsLastTaxisLocation 
 
 urlpatterns = [
     path('taxis/', viewsTaxis.listTaxis, name='get_all_taxis'),
     path('trajectories/', viewsTrajectories.listTrajectories, name='get_all_trajectories'),
-    path('lastlocation/', viewsLastTaxisLocation.last_taxis_location, name='get_all_last_location'),
+    path('lastlocation/', viewsLastTaxisLocation.lastTaxisLocation, name='get_all_last_location'),
 ]
--- fleet_management_app/views/viewsLastTaxisLocation.py
+++ fleet_management_app/views/viewsLastTaxisLocation.py
@@ -5,7 +5,7 @@ from rest_framework import status
 
 from ..serializers import TrajectoriesSerializer
 from ..schemas import last_taxis_location_schema
-from ..utils import LastLocationsUtils
+from ..utils import LastTaxisLocationUtils
 
 import json
 
@@ -13,13 +13,13 @@ import json
 
 # Function to list the last location of all taxis
 @api_view(['GET'])
-def last_taxis_location(request):
+def lastTaxisLocation(request):
     """
     Get the last location of each taxi with optional filtering, sorting, searching, and pagination.
     """
     if request.method == 'GET':
         # Instantiate LastLocationsUtils class with the request object
-        last_locations_utils = LastLocationsUtils(request)
+        last_locations_utils = LastTaxisLocationUtils(request)
         
         # Get request query parameters
         filter_by = request.query_params.get('filter_by')
@@ -33,10 +33,6 @@ def last_taxis_location(request):
         # Get the latest locations of all taxis
         last_trajectories = last_locations_utils.get_taxis_last_location()
 
-        # Check if the list of taxis is empty
-        if not last_trajectories:
-            return Response({'detail': 'Nenhum táxi encontrado.'}, status=status.HTTP_404_NOT_FOUND)
-
         # Filter, sort and search trajectories
         last_trajectories = last_locations_utils.filter_last_locations(last_trajectories, filter_by)
         last_trajectories = last_locations_utils.sort_last_locations(last_trajectories, sort_by)
@@ -46,6 +42,10 @@ def last_taxis_location(request):
         paginator = PageNumberPagination()
         paginator.page_size = page_size
         result_page = paginator.paginate_queryset(last_trajectories, request)
+
+        # Check if the list of taxis is empty
+        if not last_trajectories:
+            return Response({'detail': 'Nenhum táxi encontrado.'}, status=status.HTTP_404_NOT_FOUND)
         
         # Serialize the paginated queryset
         serializer = TrajectoriesSerializer(result_page, many=True)
@@ -66,7 +66,7 @@ def last_taxis_location(request):
     return Response({'detail': 'Por favor, forneça um taxi_id válido'}, status=status.HTTP_400_BAD_REQUEST)
 
 # Esquema personalizado para documentar a visualização de última localização do táxi no Swagger
-last_taxis_location = last_taxis_location_schema(method='get')(last_taxis_location)
+lastTaxisLocation = last_taxis_location_schema(method='get')(lastTaxisLocation)
 
 
 
